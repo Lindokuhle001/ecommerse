5a075505f0173905c4c086d9a4b9eb8172d69e95
lindokuhle shabalala <mlixshaba@gmail.com>
2023-06-17 14:37:56 +0200

    connected db

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = 5000;
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 
+dbConnect();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+app.get('/',(req,res)=> 'hello')
 
 app.listen(PORT, () => {
   console.log(`Server is running  at PORT ${PORT}`);
